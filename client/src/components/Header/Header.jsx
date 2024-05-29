d6cdedfa9cf37aea1b0cfdaf5652d0ffc27cfc29
Steven Ho <steven.ho01@outlook.com>
2024-05-29 10:11:40 -0400

    Ajout d'un header basse fidélité

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <img src={logo} alt="To-Do" />
+            </div>
+            <search>
+                <form>
+                    <input type="text" placeholder="Search task" />
+                    <button>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+            </search>
+            <div className="profile-menu">
+                <button>James Reece</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
