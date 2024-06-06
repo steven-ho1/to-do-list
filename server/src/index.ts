ebe5b565323b07ff0b816daa6b0e8b9d3712e1a4
Steven Ho <steven.ho01@outlook.com>
2024-06-06 18:09:14 -0400

    Réarrangement de configurations et début d'un serveur avec Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("test");
+import express from "express";
+import { Request, Response } from "express";
+import http from "http";
+
+const app = express();
+
+app.use(express.json());
+
+// test
+app.get("/", (req: Request, res: Response) => {});
+
+http.createServer(app).listen(3000);
+console.log("Démarré");
